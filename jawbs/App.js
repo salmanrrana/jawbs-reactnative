cbf32058115de2a5992f1e4344ca6816fc33879c
Salman Rana <salman.r.rana@gmail.com>
2017-06-25 18:46:59 -0500

    temp fix for settings button and to go back

--- jawbs/App.js
+++ jawbs/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
-
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
     });
 
     return (
       <View style={styles.container}>
-        <MainNavigator />
+        <MainNavigator screenProps="navigate"/>
       </View>
     );
   }
@@ -25,7 +40,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
--- jawbs/screens/ReviewScreen.js
+++ jawbs/screens/ReviewScreen.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation'
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+
 
 class ReviewScreen extends Component {
+  // static navigationOptions = {
+  //   title: 'Review Jobs',
+  //   header: ({ navigate }) => {
+  //     return {
+  //       HeaderRight:   <Button title="Settings" onPress={() => navigate('settings')} />
+  //     };
+  //   }
+  // }
+  static navigationOptions = {
+    title: 'Review Jobs'
+  }
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
+        <Button
+          title="Settings"
+          onPress={() => navigate('settings')}
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(1,122,255,1)"
+        />
         <Text>ReviewScreen</Text>
         <Text>ReviewScreen</Text>
         <Text>ReviewScreen</Text>
@@ -18,3 +40,17 @@ class ReviewScreen extends Component {
 }
 
 export default ReviewScreen;
+//
+// Hi Stephen,
+//
+// Sorry to bother again, but I'm having issues with react navigation.
+//
+// Im just on the beginning of the lesson for the jobs app and I'm having issues with the navigationOption and adding navigate to right in ReviewScreen.js
+//
+// When reading through the documentation it looks like it is now headerRight but then when trying to pass the =>  in the button it is saying that i am unable to pass an object.
+//
+// my repo is still connected above.
+//
+// as a temporary solution I added the button to the main render()
+//
+// thanks so much for your help and I look forward to hearing from you.
